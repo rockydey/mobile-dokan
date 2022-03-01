3e7cfc6f6f486841072e17c6b4144774d71cb806
rockydey <coderrocky25@gmail.com>
2022-03-01 10:41:18 +0600

    reduce extra code and make mobile friendly

--- js/mobile_dokan.js
+++ js/mobile_dokan.js
@@ -32,9 +32,9 @@ const displaySearchResult = phones => {
             const div = document.createElement('div');
             div.classList.add('col');
             div.innerHTML = `
-            <div class="card">
-                <img src="${phone.image}" class="card-img-top" alt="${phone.phone_name}">
-                <div class="card-body">
+            <div class="card align-items-center">
+                <img src="${phone.image}" class="card-img-top w-50 mt-2" alt="${phone.phone_name}">
+                <div class="card-body d-flex flex-column align-items-center">
                     <h5 class="card-title">Phone: ${phone.phone_name}</h5>
                     <p class="card-text fw-bold fs-5">Brand: ${phone.brand}</p>
                     <button onclick="showDetail('${phone.slug}')" class="btn btn-success">Show Details</button>
@@ -72,28 +72,21 @@ const displayDetails = detail => {
 
     const div = document.createElement('div');
     div.classList.add('card');
-    if (detail.releaseDate == "") {
-        div.innerHTML = `
-            <img src="${detail.image}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h6 class="card-title">Release Date: <span class="text-danger">Sorry, release date details not available.</span></h6>
-                <h6 class="card-text">Chipset: <span class="fw-normal">${detail.mainFeatures.chipSet}</span></h6>
-                <h6 class="card-text">Storage: <span class="fw-normal">${detail.mainFeatures.storage}</span></h6>
-                <h6 class="card-text">Display: <span class="fw-normal">${detail.mainFeatures.displaySize}</span></h6>
-                <h6 class="card-text">Sensors: <span class="fw-normal">${[...detail.mainFeatures.sensors]}</span></h6>            
-            </div>
-        `;
-    } else {
-        div.innerHTML = `
-            <img src="${detail.image}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h6 class="card-title">Release Date: <span class="fw-normal">${detail.releaseDate}</span></h6>
-                <h6 class="card-text">Chipset: <span class="fw-normal">${detail.mainFeatures.chipSet}</span></h6>
-                <h6 class="card-text">Storage: <span class="fw-normal">${detail.mainFeatures.storage}</span></h6>
-                <h6 class="card-text">Display: <span class="fw-normal">${detail.mainFeatures.displaySize}</span></h6>
-                <h6 class="card-text">Sensors: <span class="fw-normal">${[...detail.mainFeatures.sensors]}</span></h6>
-            </div>
-        `;
-    }
+    div.innerHTML = `
+        <img src="${detail.image}" class="card-img-top w-75 mx-auto mt-2" alt="...">
+        <div class="card-body">
+            <h6 class="card-title">Release Date: ${detail.releaseDate !== '' ? detail.releaseDate : '<span class="text-danger">Sorry, release date details not available.</span>'}</h6>
+            <h6 class="card-text">Chipset: <span class="fw-normal">${detail.mainFeatures.chipSet}</span></h6>
+            <h6 class="card-text">Storage: <span class="fw-normal">${detail.mainFeatures.storage}</span></h6>
+            <h6 class="card-text">Display: <span class="fw-normal">${detail.mainFeatures.displaySize}</span></h6>
+            <h6 class="card-text">Bluetooth: <span class="fw-normal">${detail?.others?.Bluetooth}</span></h6>
+            <h6 class="card-text">GPS: <span class="fw-normal">${detail?.others?.GPS}</span></h6>
+            <h6 class="card-text">NFC: <span class="fw-normal">${detail?.others?.NFC}</span></h6>
+            <h6 class="card-text">Radio: <span class="fw-normal">${detail?.others?.Radio}</span></h6>
+            <h6 class="card-text">USB: <span class="fw-normal">${detail?.others?.USB}</span></h6>
+            <h6 class="card-text">WLAN: <span class="fw-normal">${detail?.others?.WLAN}</span></h6>
+            <h6 class="card-text">Sensors: <span class="fw-normal">${detail.mainFeatures.sensors}</span></h6>            
+        </div>
+    `;
     displayDetail.appendChild(div);
 };
